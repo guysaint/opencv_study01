072332f4c4f249ed09ef7cbb4106fe5422134978
guysaint <guysaint@naver.com>
2025-07-28 13:14:20 +0900

    이미지 리사이징 스터디 완료

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,7 +1,21 @@
 import cv2
-#이미지 출력
-image = cv2.imread('../image/like_lenna.png')
-cv2.imshow('Image Window', image)
+import numpy as np
 
+new_height = 300
+new_width = 300
+
+#이미지 출력
+image = cv2.imread('../image/like_lenna.png') #이미지 가져오기
+#dst = np.zeros((new_height, new_width, 3), dtype=np.uint8) #항상 uint8 형식의 이미지만 가능
+dst = np.zeros((new_height, new_width, 3), dtype=image.dtype) #이미지 형식이 무엇인지 모를 때 유연하게 원본을 따라가주는 코드
+resized = cv2.resize(image, (new_width, new_height))
+np.copyto(dst,resized)
+
+#cv2.imshow('Image Window', image) #이미지를 보여주는 명령어
+cv2.imshow('Image Window', dst)
+#cv2.imshow('Image Window', image_small)
+print(image.shape)
+print(dst.shape)
+#print(image_small.shape)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
